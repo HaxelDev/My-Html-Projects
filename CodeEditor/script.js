ccdca4221c723528b640b9f12096a3c7824a8650
Gidk-g <devhaxel@gmail.com>
2023-09-15 22:18:02 +0200

    CodeEditor

---
+++ CodeEditor/script.js
@@ -0,0 +1,25 @@
+let originalConsoleLog = console.log;
+
+function runCode() {
+    const code = document.getElementById('editor').value;
+    const consoleOutput = document.getElementById('console');
+
+    consoleOutput.textContent = '';
+
+    try {
+        console.log = function(...args) {
+            consoleOutput.textContent += args.join(' ') + '\n';
+            originalConsoleLog.apply(console, args);
+        };
+        eval(code);
+    } catch (error) {
+        console.error(error);
+        consoleOutput.textContent += 'Błąd: ' + error.message + '\n';
+    }
+}
+
+function resetConsole() {
+    const consoleOutput = document.getElementById('console');
+    consoleOutput.textContent = '';
+    console.log = originalConsoleLog;
+}
