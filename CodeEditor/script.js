d82d6e11c08a603e20485ec86a5fa8892855946d
Gidk-g <devhaxel@gmail.com>
2023-10-20 15:52:22 +0200

    file system

--- CodeEditor/script.js
+++ CodeEditor/script.js
@@ -1,3 +1,4 @@
+let files = {};
 let originalConsoleLog = console.log;
 
 function runCode() {
@@ -22,4 +23,75 @@ function resetConsole() {
     const consoleOutput = document.getElementById('console');
     consoleOutput.textContent = '';
     console.log = originalConsoleLog;
+}
+
+function createFile() {
+    const overlay = document.getElementById('overlay');
+    overlay.style.display = 'flex';
+    const createButton = document.getElementById('create-button');
+    const cancelButton = document.getElementById('cancel-button');
+    const fileNameInput = document.getElementById('file-name');
+
+    createButton.addEventListener('click', () => {
+        const fileName = fileNameInput.value;
+        if (fileName) {
+            if (!files[fileName]) {
+                files[fileName] = '';
+                showMessage('File created successfully.', false);
+                updateFileSelect();
+                overlay.style.display = 'none';
+            } else {
+                showMessage('File with that name already exists.', true);
+            }
+        } else {
+            showMessage('File creation canceled.', true);
+            overlay.style.display = 'none';
+        }
+    });
+
+    cancelButton.addEventListener('click', () => {
+        showMessage('File creation canceled.', true);
+        overlay.style.display = 'none';
+    });
+}
+
+function updateFileSelect() {
+    const fileSelect = document.getElementById('fileSelect');
+    fileSelect.innerHTML = '<option value="" disabled selected>Select a File</option>';
+    for (const fileName in files) {
+        const option = document.createElement('option');
+        option.value = fileName;
+        option.textContent = fileName;
+        fileSelect.appendChild(option);
+    }
+}
+
+function loadFile() {
+    const fileSelect = document.getElementById('fileSelect');
+    const selectedFile = fileSelect.value;
+    if (selectedFile) {
+        document.getElementById('editor').value = files[selectedFile];
+    }
+}
+
+function showMessage(message, isError = false) {
+    const messageDiv = document.createElement('div');
+    messageDiv.className = isError ? 'error-message' : 'success-message';
+    messageDiv.textContent = message;
+    document.body.appendChild(messageDiv);
+
+    setTimeout(() => {
+        document.body.removeChild(messageDiv);
+    }, 3000);
+}
+
+function saveFile() {
+    const fileSelect = document.getElementById('fileSelect');
+    const selectedFile = fileSelect.value;
+    if (selectedFile) {
+        files[selectedFile] = document.getElementById('editor').value;
+        showMessage('File saved successfully.', false);
+    } else {
+        showMessage('Please select a file first.', true);
+    }
 }
